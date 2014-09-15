b4f14670a2348e47bae9c4703d20eba75498cf89
Wataru Noguchi <wnoguchi.0727@gmail.com>
2014-09-15 06:36:11 +0000

    Implement Remote IP fetch action.

--- app/controllers/v1/network/ip_controller.rb
+++ app/controllers/v1/network/ip_controller.rb
@@ -1,4 +1,9 @@
 class V1::Network::IpController < ApplicationController
   def ipv4
+    remote_ip = request.env["HTTP_X_FORWARDED_FOR"] || request.remote_ip
+    data = { ipv4: remote_ip }
+    respond_to do |format|
+      format.json { render :json => data }
+    end
   end
 end
